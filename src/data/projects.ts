d6118b0dc8e7a89b51329c71adbde571e5dcc72c
MilosRandjelovic <mrandjelovic1979@gmail.com>
2024-12-10 10:49:01 +0100

    final changes

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,21 +2,21 @@ import { type Project } from '../types';
 
 export const projects: Project[] = [
   {
-    title: 'E-commerce Platform',
-    description: 'A full-featured online shopping platform with real-time inventory management.',
-    image: 'https://images.unsplash.com/photo-1557821552-17105176677c?w=800&auto=format&fit=crop&q=60',
-    technologies: ['React', 'Node.js', 'MongoDB', 'Tailwind CSS']
+    title: 'React-Admin-Dashboard',
+    description: 'Responsive modern React dashboard with many functions and charts.',
+    image: '/admin-dashboard.png',
+    technologies: ['React', 'Framer Motion', 'Recharts', 'Tailwind CSS']
   },
   {
-    title: 'Task Management App',
-    description: 'Collaborative task management tool with real-time updates and team features.',
-    image: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?w=800&auto=format&fit=crop&q=60',
-    technologies: ['React', 'Firebase', 'Material-UI']
+    title: 'Movie Catalog',
+    description: 'Movie app similar to IMDB with search functionality.',
+    image: '/movie-catalog.png',
+    technologies: ['Next.js', 'Tailwind CSS', 'Next Themes']
   },
   {
-    title: 'Weather Dashboard',
-    description: 'Interactive weather application with detailed forecasts and map integration.',
-    image: 'https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?w=800&auto=format&fit=crop&q=60',
-    technologies: ['React', 'OpenWeather API', 'Chart.js']
+    title: 'QR Code Generator',
+    description: 'QR code generator with possibility to download as image.',
+    image: '/qr-code-generator.png',
+    technologies: ['Next.js', 'Typescript', 'Shadcn UI', 'Tailwind CSS']
   }
 ];
